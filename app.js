0e8bd69083b3d6a19c6fe51809f5ec35b5fa3004
JoseTorres2210 <86307604+JoseTorres2210@users.noreply.github.com>
2023-12-18 09:50:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+ /* MÓDULO dotenv */
+ const dotenv = require('dotenv');
+
+ /* CARGA DE DATOS DE CONFIGURACION EN MEMORIA */
+ dotenv.config();
+ 
 var app = express();
 
 // view engine setup
